12fc8225851638fa8e269f630a72cc8a3139a3e1
Ioannis Canellos <iocanel@gmail.com>
2018-02-09 16:34:27 +0200

    refactor: switch to system managed jvm.

--- .zshenv.d/20-development.zsh
+++ .zshenv.d/20-development.zsh
@@ -3,9 +3,11 @@ export GOPATH=$HOME/workspace
 export GO15VENDOREXPERIMENT=1
 
 #Java 1.7.0
-export JAVA_HOME=$HOME/tools/oracle-jdk-7
+#export JAVA_HOME=$HOME/tools/oracle-jdk-7
 #Java 1.8.0
-export JAVA_HOME=$HOME/tools/oracle-jdk-8
+#export JAVA_HOME=$HOME/tools/oracle-jdk-8
+
+export JAVA_HOME=/usr/lib/jvm/default
 
 #Java Tooling
 export JAVA_OPTS=-Xmx1024m
@@ -13,4 +15,5 @@ export GRADLE_HOME=$HOME/tools/gradle
 export M2_HOME=$HOME/tools/maven
 export VERTX_HOME=$HOME/tools/vertx
 
-export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$VERTX_HOME/bin:$GOPATH/bin:$PATH
+#export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$VERTX_HOME/bin:$GOPATH/bin:$PATH
+export PATH=$M2_HOME/bin:$VERTX_HOME/bin:$GOPATH/bin:$PATH
