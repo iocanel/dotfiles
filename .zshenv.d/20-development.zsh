e8f8ede313888290b0b86e4215b911a4ce40f87a
Ioannis Canellos <iocanel@gmail.com>
2018-02-28 10:27:22 +0200

    chore: cleanup 20-development.zsh

--- .zshenv.d/20-development.zsh
+++ .zshenv.d/20-development.zsh
@@ -2,18 +2,12 @@
 export GOPATH=$HOME/workspace
 export GO15VENDOREXPERIMENT=1
 
-#Java 1.7.0
-#export JAVA_HOME=$HOME/tools/oracle-jdk-7
-#Java 1.8.0
-#export JAVA_HOME=$HOME/tools/oracle-jdk-8
-
-export JAVA_HOME=/usr/lib/jvm/default
 
 #Java Tooling
+export JAVA_HOME=/usr/lib/jvm/default
 export JAVA_OPTS=-Xmx1024m
 export GRADLE_HOME=$HOME/tools/gradle
 export M2_HOME=$HOME/tools/maven
 export VERTX_HOME=$HOME/tools/vertx
 
-#export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$VERTX_HOME/bin:$GOPATH/bin:$PATH
 export PATH=$M2_HOME/bin:$VERTX_HOME/bin:$GOPATH/bin:$PATH
