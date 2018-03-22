dc228007443fc98771ecfac4c0fd6adad074d0d7
Ioannis Canellos <iocanel@gmail.com>
2018-03-22 14:46:48 +0200

    chore: latest generated changes in init.el.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -24,11 +24,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+    ("f770ddb6b7dfda47ca06531a81d8d311077b93ebf8f54309548009faf362895c" "065efdd71e6d1502877fd5621b984cded01717930639ded0e569e1724d058af8" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(magit-gh-pulls-arguments (quote ("--open-new-in-browser")))
+ '(magit-pull-arguments nil)
+ '(magit-push-arguments (quote ("--force")))
  '(package-selected-packages
    (quote
-    (swiper ivy go-eldoc hydra company-emacs-eclim git-gutter-fringe git-timemachine pdf-tools yasnippet-snippets rainbow-mode esh-autosuggest ## expand-region mark-multiple popup-kill-ring company-jedi virtualenvwarpper flycheck virtualenvwrapper pytest smart-tab emmet-mode ob-python ob-go ox-gfm ox-asciidoc github-pullrequest github-issues magit-gh-pulls magit-gh-pull org-present ox-reveal anaconda-mode pyenv-mode python-mode yaml-mode protobuf-mode kotlin-mode groovy-mode dockerfile-mode yasnippet projectile dashboard rainbow-delimiters company-go go-mode org2blog org-bullets magit ido-vertical-mode smart-mode-line-powerline-theme darcula-theme sudo-edit linum-relative company which-key evil-leader use-package))))
+    (flx-ido go-projectile meghanada tide xresources-theme html2org swiper ivy go-eldoc hydra git-gutter-fringe git-timemachine pdf-tools yasnippet-snippets rainbow-mode esh-autosuggest ## expand-region mark-multiple popup-kill-ring company-jedi virtualenvwarpper flycheck virtualenvwrapper pytest smart-tab emmet-mode ob-python ob-go ox-gfm ox-asciidoc github-pullrequest github-issues magit-gh-pulls magit-gh-pull org-present ox-reveal anaconda-mode pyenv-mode python-mode yaml-mode protobuf-mode kotlin-mode groovy-mode dockerfile-mode yasnippet projectile dashboard rainbow-delimiters company-go go-mode org2blog org-bullets magit ido-vertical-mode smart-mode-line-powerline-theme darcula-theme sudo-edit linum-relative company which-key evil-leader use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
