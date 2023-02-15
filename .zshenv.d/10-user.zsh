4e5287295655289070a9b224698679b5acd56634
Ioannis Canellos <iocanel@gmail.com>
2023-02-15 13:47:35 +0200

    chore: update zshenv

--- .zshenv.d/10-user.zsh
+++ .zshenv.d/10-user.zsh
@@ -1,4 +1,9 @@
+export EDITOR=vi
+export TERMINAL=alacritty
+
 #Scripts
 SCRIPTS_HOME=$HOME/scripts
 
-export PATH=${SCRIPTS_HOME}/arch:${SCRIPTS_HOME}:${SCRIPTS_HOME}/dev:${SCRIPTS_HOME}/install:${SCRIPTS_HOME}/k8s:${SCRIPTS_HOME}/notify:${SCRIPTS_HOME}/sound:${SCRIPTS_HOME}/util:${SCRIPTS_HOME}/wm:${SCRIPTS_HOME}/work:${SCRIPTS_HOME}/wrap:/bin:/usr/bin:/usr/local/bin:/var/lib/snapd/snap/bin:$HOME/bin
+export PATH=$HOME/.local/bin:${SCRIPTS_HOME}/arch:${SCRIPTS_HOME}:${SCRIPTS_HOME}/dev:${SCRIPTS_HOME}/install:${SCRIPTS_HOME}/k8s:${SCRIPTS_HOME}/notify:${SCRIPTS_HOME}/sound:${SCRIPTS_HOME}/util:${SCRIPTS_HOME}/wm:${SCRIPTS_HOME}/work:${SCRIPTS_HOME}/wrap:/bin:/usr/bin:/usr/local/bin:/var/lib/snapd/snap/bin:$HOME/bin
+
+export HG2JJ_DIR=~/.local/share/hg2jj/
