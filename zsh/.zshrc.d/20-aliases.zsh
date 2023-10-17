21eb1c29fde0ab706f53586df9d152fa1f1c6ee9
Ioannis Canellos <iocanel@gmail.com>
2023-10-17 16:48:33 +0300

    chore: tune alias for quarkus and dekorate cli

--- zsh/.zshrc.d/20-aliases.zsh
+++ zsh/.zshrc.d/20-aliases.zsh
@@ -4,4 +4,5 @@ alias shutdown='shutdown -hP now'
 alias ps-rss='ps ax -o pid,rss,command | numfmt --header --from-unit=1024 --to=iec --field 2|grep -v grep'
 alias asciicast2gif='docker run --rm -v $PWD:/data asciinema/asciicast2gif'
 alias idea="~/Idea/intellij/bin/idea.sh nosplash"
-alias quarkus="java -jar ~/workspace/src/github.com/quarkusio/quarkus/devtools/cli/target/quarkus-cli-999-SNAPSHOT-runner.jar"
+alias qs="java -jar ~/workspace/src/github.com/quarkusio/quarkus/devtools/cli/target/quarkus-cli-999-SNAPSHOT-runner.jar"
+alias dekorate="java -jar ~/workspace/src/github.com/dekorateio/dekorate-cli/target/dekorate-cli-999-SNAPSHOT-runner.jar"
