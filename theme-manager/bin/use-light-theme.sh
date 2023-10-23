753048591b3a66755710b8fcad0cfa0a48f07f4c
Ioannis Canellos <iocanel@gmail.com>
2023-10-23 09:29:43 +0300

    feat: add first draft of theme manager scripts

---
+++ theme-manager/bin/use-light-theme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Alacrity
+pushd ~/.config/alacritty/themes
+if [ -f current-theme.yaml ]; then
+  unlink current-theme.yaml
+fi
+ln -s light-theme.yaml current-theme.yaml
+popd
+touch ~/.config/alacritty/alacritty.yml
+
+# Neovim
+pushd ~/.config/nvim/lua/plugins
+if [ -f current-theme.lua ]; then
+  unlink current-theme.lua
+fi
+ln -s ../themes/light-theme.lua  current-theme.lua
+popd
