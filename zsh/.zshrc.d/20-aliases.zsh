8d0afc65d3ed8194bbc9b24fc38ba7aec041c4bc
Ioannis Canellos <iocanel@gmail.com>
2023-12-18 09:25:13 +0200

    chore: add dev aliases

--- zsh/.zshrc.d/20-aliases.zsh
+++ zsh/.zshrc.d/20-aliases.zsh
@@ -5,4 +5,8 @@ alias ps-rss='ps ax -o pid,rss,command | numfmt --header --from-unit=1024 --to=i
 alias asciicast2gif='docker run --rm -v $PWD:/data asciinema/asciicast2gif'
 alias idea="~/Idea/intellij/bin/idea.sh nosplash"
 alias qs="java -jar ~/workspace/src/github.com/quarkusio/quarkus/devtools/cli/target/quarkus-cli-999-SNAPSHOT-runner.jar"
+alias qton="java -jar ~/workspace/src/github.com/iocanel/qton/target/qton-999-SNAPSHOT-runner.jar"
+alias njq="java -jar ~/workspace/src/github.com/iocanel/njq/target/njq-999-SNAPSHOT-runner.jar"
 alias dekorate="java -jar ~/workspace/src/github.com/dekorateio/dekorate-cli/target/dekorate-cli-999-SNAPSHOT-runner.jar"
+alias kubevirt="java -jar ~/workspace/src/github.com/quarkiverse/quarkus-kubevirt-client/cli/target/quarkus-kubevirt-cli-999-SNAPSHOT-runner.jar"
+alias openshift-vm="java -jar ~/workspace/src/github.com/quarkiverse/quarkus-kubevirt-client/cli/target/quarkus-openshift-vm-cli-999-SNAPSHOT-runner.jar"
